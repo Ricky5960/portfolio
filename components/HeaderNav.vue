18e2b18bc9caebb5fd936e66418524362a9afcfa
Tatsuya Kobayashi <t04.ty0425@gmail.com>
2018-09-18 23:51:27 +0900

    change component

--- components/HeaderNav.vue
+++ components/HeaderNav.vue
@@ -1,29 +1,35 @@
 <template>
-  <header>
-    <nav>
-      <ul class="cf">
-        <li class="home"><nuxt-link to="/">Home</nuxt-link></li>
-        <li class="about"><nuxt-link to="/about">About</nuxt-link></li>
-        <li class="works"><nuxt-link to="/works">Works</nuxt-link></li>
-        <li class="contact"><nuxt-link to="/contact">Contact</nuxt-link></li>
-      </ul>
-    </nav>
-    <ul class="socialBtn">
-      <li>
-        <a href="https://twitter.com/t04_tatsuya" target="_blank">
-          <img src="img/sns/Twitter_Logo_Blue.png" alt="Twitter">
-        </a>
-      </li>
-      <li>
-        <a href="https://www.wantedly.com/users/18153987" target="_blank">
-          <img src="img/sns/wantedly_mark.png" alt="wantedry">
-        </a>
-      </li>
-      <li lass="last">
-        <a href="https://github.com/kokokoba" target="_blank">
-          <img src="img/sns/GitHub-Mark-32px.png" alt="GitHub"></a>
-      </li>
-    </ul>
+  <header class="hero is-medium">
+    <div class="hero-head">
+      <nav class="navbar">
+        <div class="container">
+          <div class="navbar-brand">
+            <a href="https://twitter.com/t04_tatsuya" target="_blank" class="navbar-item">
+              <img src="img/sns/Twitter_Logo_Blue.png" alt="Twitter">
+            </a>
+            <a href="https://www.wantedly.com/users/18153987" target="_blank" class="navbar-item">
+              <img src="img/sns/wantedly_mark.png" alt="wantedry">
+            </a>
+            <a href="https://github.com/kokokoba" target="_blank" class="navbar-item">
+              <img src="img/sns/GitHub-Mark-32px.png" alt="GitHub">
+            </a>
+            <span class="navbar-burger burger" data-target="navbarMenuHeroA">
+            <span></span>
+            <span></span>
+            <span></span>
+          </span>
+          </div>
+          <div id="navbarMenuHeroA" class="navbar-menu">
+            <div class="navbar-end">
+              <nuxt-link to="/" class="home navbar-item">Home</nuxt-link>
+              <nuxt-link to="/about" class="about navbar-item">About</nuxt-link>
+              <nuxt-link to="/works" class="works navbar-item">Works</nuxt-link>
+              <nuxt-link to="/contact" class="contact navbar-item">Contact</nuxt-link>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
   </header>
 </template>
 
