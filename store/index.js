7036f60db674ec7e991970c346d207d85d696f6a
kobayashi_tatsuya@oisixdotdaichi.co.jp <kobayashi_tatsuya@oisixdotdaichi.co.jp>
2018-09-19 17:35:17 +0900

    change index.js

--- store/index.js
+++ store/index.js
@@ -1,14 +1,14 @@
-import Vuex from 'vuex'
-
-const store = () => new Vuex.Store({
-  state: {
-    counter: 0
-  },
-  mutations: {
-    increment (state) {
-      state.counter++
-    }
-  }
-});
-
-export default store
+// import Vuex from 'vuex'
+//
+// const store = () => new Vuex.Store({
+//   state: {
+//     counter: 0
+//   },
+//   mutations: {
+//     increment (state) {
+//       state.counter++
+//     }
+//   }
+// });
+//
+// export default store
